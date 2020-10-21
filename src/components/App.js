a2fc8d338b82be5ff332954f7cec3b60e2bf6b4b
Preeti Sahani <43055616+preetisahani21@users.noreply.github.com>
2020-10-21 20:27:55 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React, {Component, useState} from "react";
+import { render } from "enzyme";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isClicked, setisClicked] = useState(false);
+  const handleClick = () => {
+    const makeIttrue = true;
+    setisClicked(makeIttrue);
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>
+        click
+      </button>
+      {isClicked && (
+        <pre id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </pre>
+      )}
     </div>
   );
 }
-
 
 export default App;
